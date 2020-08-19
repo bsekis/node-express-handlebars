2cd40e17ac838622566258b310603283ba4ec36a
Ben <bsekis21@gmail.com>
2020-08-19 15:13:28 -0400

    changed port

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ const connection = process.env.JAWSDB_URL
   ? mysql.createConnection(process.env.JAWSDB_URL)
   : mysql.createConnection({
     host: 'us-cdbr-east-02.cleardb.com',
-    port: 3306,
+    port: process.env.PORT || 3306,
     user: bc0dae49105ef7,
     password: acb52a99,
     database: heroku_4f4d3a323adc514
